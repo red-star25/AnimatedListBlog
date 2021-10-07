fe051bcdc1091f8f5228f3fb361f41870c81e045
Dhruv Nakum <54853762+red-star25@users.noreply.github.com>
2021-10-07 19:28:50 +0530

    Create data.dart

---
+++ AnimatedListFinal/data.dart
@@ -0,0 +1,13 @@
+import 'package:animation_blog/model/todo_item.dart';
+
+class Data {
+  static final List<Todo> todoList = [
+    Todo(title: "Buy a new macbook", isCompleted: false),
+    Todo(title: "Get a feedback on website design", isCompleted: false),
+    Todo(title: "Buy milk", isCompleted: false),
+    Todo(title: "Call katherina about the trip", isCompleted: false),
+    Todo(title: "Pay bills", isCompleted: false),
+    Todo(title: "Organize office", isCompleted: false),
+    Todo(title: "Read a book", isCompleted: false),
+  ];
+}
